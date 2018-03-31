81d882df52ef70c59dc0cbebc2fd29e99016b809
HuuBaa <742790905@qq.com>
2018-03-31 20:07:26 +0800

    M T V

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -2,9 +2,10 @@
 #-*- coding: utf-8 -*-
 
 from django.conf.urls import url
-from rango import views
+from . import views
 
 urlpatterns=[
-    url(r'^about/',views.about,name='about'),
-    url(r'^$',views.index,name='index')
+    url(r'^about/$',views.about,name='about'),
+    url(r'^$',views.index,name='index'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)$',views.show_category,name='show_category')
 ]
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Category,Page
 
 def index(request):
-    con_dict={'boldmessage':'我 要 吃 鸡 腿 ！'}
+    category_list_liked=Category.objects.order_by('-likes')[:5]
+    page_list_viewed=Page.objects.order_by('-views')[:5]
+    con_dict={'categories_liked':category_list_liked,'page_list_viewed':page_list_viewed}
     return render(request,'rango/index.html',context=con_dict)
+
+def show_category(request,category_name_slug):
+    con_dict={}
+    try:
+        category=Category.objects.get(slug=category_name_slug)
+        pages=Page.objects.filter(category=category)
+        con_dict["pages"]=pages
+        con_dict["category"] = category
+
+    except Category.DoesNotExist:
+
+        con_dict["pages"] = None
+        con_dict["category"] = None
+    return render(request,'rango/category.html',con_dict)
 
 def about(request):
     return  render(request,'rango/about.html')
